4decdf5458d18ea3731209c695eecd1dc654244e
Stéphane Klein <contact@stephane-klein.info>
2022-07-09 23:59:03 +0200

    Fix svelte.config.js and vite.config.js

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,9 +1,11 @@
-<script>
-import MoonClearFill from '~icons/ri/moon-clear-fill';
+<script lang="js">
+import SvelteLogo from 'virtual:icons/logos/svelte-icon';
+import MoonClearFill from 'virtual:icons/ri/moon-clear-fill';
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 
+<SvelteLogo />
 <MoonClearFill />
 
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
